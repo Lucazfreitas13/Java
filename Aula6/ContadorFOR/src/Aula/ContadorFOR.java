ea2898f942acecc97a17ac1a9045421dd7f1f845
Lucazfreitas13 <lucas.ferreiradefreitas1995@hotmail.com>
2024-10-08 15:28:42 -0300

    Aula6_Java

---
+++ Aula6/ContadorFOR/src/Aula/ContadorFOR.java
@@ -0,0 +1,12 @@
+package Aula;
+
+public class ContadorFOR {
+
+	public static void main(String[] args) {
+		System.out.println("Usando FOR");
+		for(int contador = 0; contador <= 10;contador++) {
+			System.out.println(contador);
+		}
+	}
+
+}
---
+++ Aula6/ContadorWhile/src/Aula/ContadorWhile.java
@@ -0,0 +1,15 @@
+package Aula;
+
+public class ContadorWhile {
+
+	public static void main(String[] args) {
+		System.out.println("Usando While");
+		int contador = 0;
+		while(contador <= 10) {
+			System.out.println(contador);
+			contador = contador +1;
+		}
+
+	}
+
+}
